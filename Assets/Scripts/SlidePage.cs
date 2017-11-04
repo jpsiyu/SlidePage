a3dc9f9c6912ec9b93ca9d967a1a822b923bbdce
jpsiyu <jpsiyu@163.com>
2017-11-04 10:23:30 +0800

    bug fix

--- Assets/Scripts/SlidePage.cs
+++ Assets/Scripts/SlidePage.cs
@@ -118,7 +118,7 @@ public class SlidePage : MonoBehaviour, IBeginDragHandler, IEndDragHandler {
         float normalizeValue = scrollRect.normalizedPosition.x;
         normalizeValue = Mathf.Clamp(normalizeValue, 0f, 1f);
         d = 1.0f / (content.childCount - 1);
-        low = Mathf.RoundToInt(Mathf.Floor(normalizeValue / d));
+        low = Mathf.RoundToInt(normalizeValue / d);
         high = low + 1;
     }
 
